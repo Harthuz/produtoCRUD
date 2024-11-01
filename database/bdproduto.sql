c1682d1b761c16b92129dac6182b01609e40aa2b
Hernandes <Hernandeshass910@gmail.com>
2024-11-01 15:35:05 -0300

    fix: corrige banco de dados

--- database/bdproduto.sql
+++ database/bdproduto.sql
@@ -1,46 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Tempo de geração: 26/10/2024 às 00:30
--- Versão do servidor: 8.3.0
--- Versão do PHP: 8.2.18
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `bdproduto`
---
 CREATE DATABASE `bdproduto`;
 USE `bdproduto`;
 
--- --------------------------------------------------------
-
---
 -- Estrutura para tabela `tblproduto`
---
-
-DROP TABLE IF EXISTS `tblproduto`;
-CREATE TABLE IF NOT EXISTS `tblproduto` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tblproduto` (
+  `id` int NOT NULL,
   `nome` varchar(50) NOT NULL,
   `estoque` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM;
 
---
--- Despejando dados para a tabela `tblproduto`
---
-
+-- Inserindo dados na tabela `tblproduto`
 INSERT INTO `tblproduto` (`id`, `nome`, `estoque`) VALUES
 (6, 'asd', 12),
 (2, 'Caneta', 100),
@@ -48,26 +17,12 @@ INSERT INTO `tblproduto` (`id`, `nome`, `estoque`) VALUES
 (4, 'Smartphone', 20),
 (5, 'Mouse', 70);
 
--- --------------------------------------------------------
-
---
 -- Estrutura para tabela `usuario`
---
-
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE IF NOT EXISTS `usuario` (
+CREATE TABLE `usuario` (
   `usuario` varchar(50) NOT NULL,
   `senha` int NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM;
 
---
--- Despejando dados para a tabela `usuario`
---
-
+-- Inserindo dados na tabela `usuario`
 INSERT INTO `usuario` (`usuario`, `senha`) VALUES
 ('Hernandes', 123456);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
