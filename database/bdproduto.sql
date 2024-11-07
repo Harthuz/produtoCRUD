7500559dd8aae88caa6736ffa273ec008ee6e393
Hernandes <Hernandeshass910@gmail.com>
2024-11-07 13:46:24 -0300

    fix: corrige banco de dados sql
    - Tira campo de ID nos inserts de produto
    - Adiciona auto increment a id produto

--- database/bdproduto.sql
+++ database/bdproduto.sql
@@ -3,19 +3,18 @@ USE `bdproduto`;
 
 -- Estrutura para tabela `tblproduto`
 CREATE TABLE `tblproduto` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   `estoque` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM;
 
 -- Inserindo dados na tabela `tblproduto`
-INSERT INTO `tblproduto` (`id`, `nome`, `estoque`) VALUES
-(6, 'asd', 12),
-(2, 'Caneta', 100),
-(3, 'Caderno', 80),
-(4, 'Smartphone', 20),
-(5, 'Mouse', 70);
+INSERT INTO `tblproduto` (`nome`, `estoque`) VALUES
+('Caneta', 100),
+('Caderno', 80),
+('Smartphone', 20),
+('Mouse', 70);
 
 -- Estrutura para tabela `usuario`
 CREATE TABLE `usuario` (
